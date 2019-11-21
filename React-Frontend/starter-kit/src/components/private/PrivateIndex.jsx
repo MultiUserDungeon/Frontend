834f72506d99f6d829045d388039b4f8ad3f74ce
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-11-20 23:00:44 -0800

    Update PrivateIndex.jsx

--- React-Frontend/starter-kit/src/components/private/PrivateIndex.jsx
+++ React-Frontend/starter-kit/src/components/private/PrivateIndex.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const PrivateIndex = () => {
 	return (
 		<div className="private_index">
-			Private Index Lives Here, This is a private page, you must have an valid token in local storage to see it
+			Private Index Lives Here, This is a private page, you must have a token in local storage to see it
 		</div>
 		);
 }
