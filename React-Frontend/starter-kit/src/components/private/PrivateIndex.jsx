f13352fd6d0984086f52b9861b3abadf44b42399
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-11-21 12:57:59 -0800

    working on getting data for the room the current user is in

--- React-Frontend/starter-kit/src/components/private/PrivateIndex.jsx
+++ React-Frontend/starter-kit/src/components/private/PrivateIndex.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-const PrivateIndex = () => {
+import {connect} from 'react-redux';
+
+function PrivateIndex (props) {
+	console.log('private index props ' + props.userData)
 	return (
 		<div className="private_index">
 			Private Index Lives Here, This is a private page, you must have a token in local storage to see it
+			{props.fuck}
 		</div>
 		);
 }
- 
-export default PrivateIndex;
+
+const mapStateToProps = (state) => ({
+	userData: state.AuthenticationReducer.user2
+})
+
+export default connect(mapStateToProps, {})(PrivateIndex);
