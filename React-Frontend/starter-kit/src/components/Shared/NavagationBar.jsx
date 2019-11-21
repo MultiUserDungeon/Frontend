0847af93c83f25980c7a52ff76cbb414a3c9578b
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-11-20 22:35:58 -0800

    changed double quotes to single because the lack of uniformity killed me inside. Also added a private route for /game

--- React-Frontend/starter-kit/src/components/Shared/NavagationBar.jsx
+++ React-Frontend/starter-kit/src/components/Shared/NavagationBar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {withRouter, Link, NavLink} from "react-router-dom";
+import {withRouter, Link, NavLink} from 'react-router-dom';
 import {logout} from '../../util/actions/AuthenticationActions';
 
 function NavagationBar (props) {
 	console.log(props)
 
 	return(
-		<div className="navagation_bar">
+		<div className='navagation_bar'>
 			{ props.loggedIn ? (
 				// If loggedIn === true
-				<div className="private_nav">
+				<div className='private_nav'>
 					<NavLink to='/' className='navlink' onClick={props.logout}> Logout </NavLink>
 				</div>
 			):(
 				// if loggedIn === false
-				<div className="public_nav">
+				<div className='public_nav'>
 					Navagation Bar lives here. If youre logged in it will show something and if youre logged out youll see some buttons to log-in
-					<Link to="/"> home </Link>
-					<Link to="/login"> login </Link>
-					<Link to="/register"> register </Link>
+					<Link to='/'> home </Link>
+					<Link to='/login'> login </Link>
+					<Link to='/register'> register </Link>
 				</div>				
 			)}
 		</div>
--- React-Frontend/starter-kit/src/util/ListOfRoutes.js
+++ React-Frontend/starter-kit/src/util/ListOfRoutes.js
@@ -1,38 +1,40 @@
 import React from 'react';
-import {Route} from "react-router-dom";
+import {Route} from 'react-router-dom';
 
 // Components for Public Routes (No Auth)
-import PublicIndex from ".././components/public/PublicIndex";
-import Login from "../components/auth/Login";
-import Register from "../components/auth/Register";
+import PublicIndex from '.././components/public/PublicIndex';
+import Login from '../components/auth/Login';
+import Register from '../components/auth/Register';
 
 // Components for User Routes (Auth)
-// Stuff goes here
+import PrivateRoute from './PrivateRoute';
+import privateIndex from '../components/private/PrivateIndex'
 
-// Components for Admin Routes (Auth)
+// Components for Admin Routes (Auth + role type)
 // Stuff goes here
 
 export const ListOfPublicRoutes = () => {
 	return (
-		<div className="list_of_public_routes">
-			<Route exact path="/" component={PublicIndex} />
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
+		<div className='list_of_public_routes'>
+			<Route exact path='/' component={PublicIndex} />
+			<Route path='/login' component={Login} />
+			<Route path='/register' component={Register} />
 		</div>
 	);
 }
 
 export const ListOfUserRoutes = () => {
 	return (
-		<div className="list_of_user_routes">
-
+		<div className='list_of_user_routes'>
+			Holy shit this is super secret stuff man
+			<PrivateRoute path='/game' component={privateIndex} />
 		</div>
 	);
 }
 
 export const ListOfAdminRoutes = () => {
 	return (
-		<div className="list_of_admin_routes">
+		<div className='list_of_admin_routes'>
 
 		</div>
 	)
